c24c5d2b229a72cf7f1c045dd542a74bb72cc333
Alexo Ballester <alexander.ballester@orbium.com>
2018-10-07 16:11:33 +0800

    Components for Recipe Details

--- ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes : Recipe[] = [
+    new Recipe('A test recipe','This is a test','https://vignette.wikia.nocookie.net/bokunoheroacademia/images/7/7b/Shoto_angry.png/revision/latest?cb=20170720022932'),
     new Recipe('A test recipe','This is a test','https://vignette.wikia.nocookie.net/bokunoheroacademia/images/7/7b/Shoto_angry.png/revision/latest?cb=20170720022932')
   ];
 
