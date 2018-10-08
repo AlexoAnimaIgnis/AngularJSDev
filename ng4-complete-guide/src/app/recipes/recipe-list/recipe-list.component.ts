570c891513c2d0cea616acd38f2db916fe7db78f
Alexo Ballester <alexander.ballester@orbium.com>
2018-10-08 14:15:00 +0800

    Course Project -Components and Databinding

--- ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes : Recipe[] = [
     new Recipe('A test recipe','This is a test','https://vignette.wikia.nocookie.net/bokunoheroacademia/images/7/7b/Shoto_angry.png/revision/latest?cb=20170720022932'),
-    new Recipe('A test recipe','This is a test','https://vignette.wikia.nocookie.net/bokunoheroacademia/images/7/7b/Shoto_angry.png/revision/latest?cb=20170720022932')
+    new Recipe('Todoroki','This is not a test','https://vignette.wikia.nocookie.net/bokunoheroacademia/images/7/7b/Shoto_angry.png/revision/latest?cb=20170720022932')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- ng6-component-databinding/src/app/server/server.component.ts
+++ ng6-component-databinding/src/app/server/server.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server',
@@ -9,9 +9,16 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 export class ServerComponent implements OnInit {
   @Input('svrElement') element:{type:string, name:string, content:string};
   
-  constructor() { }
+  constructor() {
+    console.log('constructor called');
+   }
 
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called');
+    console.log('changes');
+  }
   ngOnInit() {
+    console.log('ngOnInit called!');
   }
 
 }
