d8b8ed2a61f6e8a1ba9a1fc1119004ee997fd13a
Alexo Ballester <alexander.ballester@orbium.com>
2018-10-09 21:46:37 +0800

    Fetching Route parameters reactively

--- ng4-route-proj/src/app/users/user/user.component.ts
+++ ng4-route-proj/src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -14,8 +14,16 @@ export class UserComponent implements OnInit {
   ngOnInit() {
     this.user = {
       id: this.route.snapshot.params['id'], // use to fetch data from route parameters
-      name: this.route.snapshot.paramMap['name']
+      name: this.route.snapshot.params['name']
     }
+     //params is an observable - subscribe to an event
+    this.route.params
+      .subscribe( // can take 3 arguments
+        (params: Params)=> { // functions
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 
 }
