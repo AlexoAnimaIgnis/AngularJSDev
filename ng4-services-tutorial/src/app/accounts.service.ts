fcff21d1c83c5e22a0399a364b7e96db70a436de
Alexo Ballester <alexander.ballester@orbium.com>
2018-10-09 09:33:14 +0800

    Using services for cross-component communication

--- ng4-services-tutorial/src/app/accounts.service.ts
+++ ng4-services-tutorial/src/app/accounts.service.ts
@@ -1,7 +1,8 @@
 import { LoggingService } from "./logging.service";
-import { Injectable } from "@angular/core";
+import { Injectable, EventEmitter } from "@angular/core";
 
-@Injectable() // add if you expect something injected such as this service needs log service
+// @Injectable() // add if you expect something injected such as this service needs log service
+@Injectable({providedIn:'root'}) 
 export class AccountsService {
     accounts = [
         {
@@ -17,6 +18,8 @@ export class AccountsService {
           status: 'unknown'
         }
     ];
+
+    statusUpdated = new EventEmitter<string>();
 
     constructor(private loggingService: LoggingService){
 
